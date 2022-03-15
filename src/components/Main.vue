3e843a80536caa96a9d8b1658afa0f4ea6584576
alemica97 <alessandro.micalizzi08@gmail.com>
2022-03-15 20:12:25 +0100

    Ho sostituito la lingua del film con un immagine, non so se l'ho fatto col procedimento corretto

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -12,10 +12,14 @@
               <ul>
                   <!-- creo tanti li quanti sono i film dentro l'array moviesList  -->
                   <li v-for="movie in moviesList" :key="movie.id">
-                      <span>titolo: {{ movie.title }}</span>
-                      <span>titolo originale: {{ movie.original_title }}</span>
-                      <span>lingua: {{ movie.original_language }}</span>
-                      <span>voto: {{ movie.vote_average }}</span>
+                        <span>titolo: {{ movie.title }}</span>
+                        <span>titolo originale: {{ movie.original_title }}</span>
+                        <span>lingua:
+                            <figure>
+                                <img :src="movie.original_language" :alt="movie.original_language">
+                            </figure> 
+                        </span>
+                        <span>voto: {{ movie.vote_average }}</span>
                   </li>
               </ul>
           </div>
@@ -36,21 +40,54 @@ export default {
             movieTitle: '',
         }
     },
+
+    beforeUpdate: function(){
+        this.flag();
+    },
+
     methods:{
         searchYourMovie: function(){
-            //chiamata alle API di the movie db che tramite dei query params mi restituisce determinati film
-            axios.get(`https://api.themoviedb.org/3/search/movie/?api_key=dbc2947f78dcb3af56135727bc3212eb&language=it-IT&query=${this.movieTitle}`)
-                .then(res => {
-                    // console.log(res.data.results);
-                    //metto l'arrey di oggetti(film), dentro il mio array moviesList
-                    this.moviesList = res.data.results; 
-                    // console.log(this.moviesList);
-                    console.log(this.movieTitle)
+            if(this.movieTitle !== ''){
+                //chiamata alle API di the movie db che tramite dei query params mi restituisce determinati film
+                axios.get(`https://api.themoviedb.org/3/search/movie`,{
+                    params:{
+                        api_key: 'dbc2947f78dcb3af56135727bc3212eb',
+                        query: this.movieTitle,
+                        language: 'it-IT',
+                    },
                 })
+                    .then(res => {
+                        //metto l'arrey di oggetti(film), dentro il mio array moviesList
+                        this.moviesList = res.data.results; 
+                        // console.log(this.moviesList);
+                        console.log(this.movieTitle)
+                    })
+            }else{
+                this.moviesList = [];
+            }
+            
         },
         // prova: function(){
         //     console.log(this.movieTitle)
         // }
+        flag: function(){
+            for(let i = 0; i < this.moviesList.length; i++){
+                switch(this.moviesList[i].original_language){
+                case 'en':
+                    this.moviesList[i].original_language = 'https://media.istockphoto.com/photos/flag-of-england-picture-id854115776?k=6&m=854115776&s=612x612&w=0&h=02VcR134xblIJdequli9a7IpZZX-0qPtGBnTpQZGdFY=';
+                    break;
+                case 'it':
+                    this.moviesList[i].original_language = 'https://th.bing.com/th/id/R.cf7d1bc348e7918da9c165ca33ef7cab?rik=xol8XZyZ7xVmJg&riu=http%3a%2f%2fflags.fmcdn.net%2fdata%2fflags%2fw580%2fit.png&ehk=%2f%2faQEeN1nMh%2fV5xj4%2f%2bS50ZIP3e4Yvlc5fGCNl5I%2fKY%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1';
+                    break;
+                case 'fr':
+                    this.moviesList[i].original_language = 'https://th.bing.com/th/id/R.aa26d890249473db99546bcd35839316?rik=ieAZZVEOSMRf%2bw&pid=ImgRaw&r=0';
+                    break;
+                default:
+                    this.moviesList[i].original_language;
+                }
+            }
+            
+        }
     },
 }
 </script>
@@ -58,5 +95,14 @@ export default {
 <style lang="scss" scoped>
     span{
         display: block;
+        & figure{
+            width: 20px;
+            aspect-ratio: 16/9;
+            display: inline-block;
+        }
+
+        & img {
+            max-width: 100%;
+        }
     }
 </style>
